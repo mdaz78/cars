b007f93d2a0dcd458325a634c5d2d8c163030495
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-29 12:48:47 +0530

    add bulma styling

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import "./index.css";
 import { store } from "./store/index.js";
+
+import "bulma/css/bulma.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
