5affbfff088811ea0e0a122ade6a98b94449f1b0
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-29 13:06:48 +0530

    feat: Update CarSearch component with search functionality

--- src/components/CarSearch.jsx
+++ src/components/CarSearch.jsx
@@ -1,7 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeSearchTerm } from "../store";
+
 const CarSearch = () => {
+  const dispatch = useDispatch();
+  const searchTerm = useSelector((state) => state.cars.searchTerm);
+
+  const handleSearchTermChange = (event) => {
+    const { value } = event.target;
+    dispatch(changeSearchTerm(value));
+  };
+
   return (
-    <div>
-      <h1>CarSearch</h1>
+    <div className="list-header">
+      <h3 className="title is-3">My Cars</h3>
+      <div className="search field is-horizontal">
+        <label htmlFor="search" className="label"></label>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          className="input"
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+      </div>
     </div>
   );
 };
