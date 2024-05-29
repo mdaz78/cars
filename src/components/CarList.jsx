5a91326c91229eb8b5d79f374b2c276ef9e9a4b8
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-29 12:49:00 +0530

    chore: Refactor CarList component and update formSlice

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,7 +1,29 @@
+import { useDispatch, useSelector } from "react-redux";
+import { removeCar } from "../store/slices/carsSlice";
+
 const CarList = () => {
+  const dispatch = useDispatch();
+  const cars = useSelector((state) => state.cars.data);
+
+  const handleCarDelete = (car) => {
+    dispatch(removeCar({ id: car.id }));
+  };
+
+  const renderedCars = cars.map((car) => (
+    <div className="panel" key={car.id}>
+      <p>
+        {car.name} ${car.cost}
+      </p>
+      <button className="button is-danger" onClick={() => handleCarDelete(car)}>
+        Delete
+      </button>
+    </div>
+  ));
+
   return (
-    <div>
-      <h1>CarList</h1>
+    <div className="">
+      <div>{renderedCars}</div>
+      <hr />
     </div>
   );
 };
