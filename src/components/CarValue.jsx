a2cac6103b9ecf3e98392350737803697f1eb0d8
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-29 13:21:04 +0530

    feat: show the total of all the cars being displayed on the screen

--- src/components/CarValue.jsx
+++ src/components/CarValue.jsx
@@ -1,9 +1,12 @@
+import { useSelector } from "react-redux";
+
 const CarValue = () => {
-  return (
-    <div>
-      <h1>CarValue</h1>
-    </div>
+  const totalCost = useSelector(({ cars: { searchTerm, data } }) =>
+    data
+      .filter(({ name }) => name.toLowerCase().includes(searchTerm))
+      .reduce((sum, car) => (sum += car.cost), 0)
   );
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 };
 
 export default CarValue;
