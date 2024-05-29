1423d94669691d1530d2a1635fbc88b0295f2a07
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-29 13:41:52 +0530

    chore: Refactor CarList component and update formSlice

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -3,30 +3,40 @@ import { removeCar } from "../store/slices/carsSlice";
 
 const CarList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
-    return data.filter(({ name }) =>
-      name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-  });
+  const { cars, name } = useSelector(
+    ({ forms, cars: { data, searchTerm } }) => ({
+      cars: data.filter(({ name }) =>
+        name.toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+      name: forms.name,
+    })
+  );
 
   const handleCarDelete = (car) => {
     dispatch(removeCar({ id: car.id }));
   };
 
-  const renderedCars = cars.map((car) => (
-    <div className="panel" key={car.id}>
-      <p>
-        {car.name} ${car.cost}
-      </p>
-      <button className="button is-danger" onClick={() => handleCarDelete(car)}>
-        Delete
-      </button>
-    </div>
-  ));
+  const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
+    return (
+      <div className={`panel ${bold && "bold"}`} key={car.id}>
+        <p>
+          {car.name} ${car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
 
   return (
-    <div className="">
-      <div>{renderedCars}</div>
+    <div className="car-list">
+      {renderedCars}
       <hr />
     </div>
   );
